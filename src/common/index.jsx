460d1d5698bba39f5508a41ac6c07db9f3a7f42f
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-03-24 03:04:46 -0700

    add AuthProvider

--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,4 +1,19 @@
+import React, { Fragment, createContext, useEffect, useReducer, useState } from 'react';
+import PropTypes from 'prop-types';
+
 import * as _ from 'lodash';
+
+// Okta SDKs
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security } from '@okta/okta-react';
+
+import AuthProvider from '../providers/AuthProvider/AuthContext';
+import AuthDispatchContext from '../providers/AuthProvider/AuthDispatcher';
+import { actions } from '../providers/AuthProvider/AuthReducer';
+import useAuthActions from '../hooks/useAuthActions';
+import useAuthDispatch from '../hooks/useAuthDispatch';
+import useAuthState from '../hooks/useAuthState';
+
 import {
 	Avatar,
 	Button,
@@ -9,8 +24,17 @@ import {
 	Input,
 	InputIcon,
 	MediaObject,
+	Modal,
 	Tooltip,
 } from '@salesforce/design-system-react';
+
+const Auth = {
+	Provider: AuthProvider,
+	DispatchContext: AuthDispatchContext,
+	useAuthDispatch,
+	useAuthState,
+	useAuthActions,
+};
 
 const LDS = {
 	Avatar,
@@ -22,7 +46,28 @@ const LDS = {
 	Input,
 	InputIcon,
 	MediaObject,
+	Modal,
 	Tooltip,
 };
 
-export { _, LDS };
+const Okta = {
+	Auth: OktaAuth,
+	Security,
+	toRelativeUrl,
+};
+
+export {
+	Auth,
+	React,
+	Fragment,
+	createContext,
+	useEffect,
+	useReducer,
+	useState,
+	Okta,
+	PropTypes,
+	_,
+	LDS,
+};
+
+export { default as config } from './config';
