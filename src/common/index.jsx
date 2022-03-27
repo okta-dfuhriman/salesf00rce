a7a5154ada4f93bf93251a25c37f04c7bf7ad6cc
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-03-27 02:07:40 -0700

    Update index.jsx

--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,31 +1,37 @@
 import React, { Fragment, createContext, useEffect, useReducer, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import * as _ from 'lodash';
 
 // Okta SDKs
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Security } from '@okta/okta-react';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 
 import AuthProvider from '../providers/AuthProvider/AuthContext';
 import AuthDispatchContext from '../providers/AuthProvider/AuthDispatcher';
-import { actions } from '../providers/AuthProvider/AuthReducer';
 import useAuthActions from '../hooks/useAuthActions';
 import useAuthDispatch from '../hooks/useAuthDispatch';
 import useAuthState from '../hooks/useAuthState';
 
 import {
+	AppLauncher,
+	AppLauncherExpandableSection,
 	Avatar,
 	Button,
 	Card,
 	Combobox,
+	Dropdown,
+	DropdownTrigger,
 	Icon,
 	IconSettings,
 	Input,
 	InputIcon,
 	MediaObject,
 	Modal,
+	Spinner,
 	Tooltip,
+	VerticalNavigation,
 } from '@salesforce/design-system-react';
 
 const Auth = {
@@ -37,22 +43,30 @@ const Auth = {
 };
 
 const LDS = {
+	AppLauncher,
+	AppLauncherExpandableSection,
 	Avatar,
 	Button,
 	Card,
 	Combobox,
+	Dropdown,
+	DropdownTrigger,
 	Icon,
 	IconSettings,
 	Input,
 	InputIcon,
 	MediaObject,
 	Modal,
+	Spinner,
 	Tooltip,
+	VerticalNavigation,
 };
 
 const Okta = {
 	Auth: OktaAuth,
+	LoginCallback,
 	Security,
+	SecureRoute,
 	toRelativeUrl,
 };
 
@@ -60,6 +74,7 @@ export {
 	Auth,
 	React,
 	Fragment,
+	Link,
 	createContext,
 	useEffect,
 	useReducer,
