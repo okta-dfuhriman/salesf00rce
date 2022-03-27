810938fecf4a4440d0546fe9e7f30cc71807ca9f
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-03-27 03:01:42 -0700

    Create Demo.jsx

---
+++ src/pages/Demo.jsx
@@ -0,0 +1,13 @@
+import { LDS } from '../common';
+
+const style = {
+	margin: 'auto',
+	width: '50%',
+	padding: '10px',
+};
+
+const Demo = () => {
+	return <div style={style}></div>;
+};
+
+export default Demo;
