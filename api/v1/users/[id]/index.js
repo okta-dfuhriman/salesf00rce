1d182cd0d840f16e0882bab1e2fc3314770ee3ca
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-03-29 20:10:07 -0700

    add getUser

--- api/v1/users/[id]/index.js
+++ api/v1/users/[id]/index.js
@@ -1,4 +1,48 @@
+import { ApiError } from '../../../_error';
 import Utils from '../../../_utils';
+
+const LINKED_OBJECT_NAME = process.env.LINKED_OBJECT_NAME;
+
+const getLinkedProfiles = async (id, unifiedId) => {
+	const client = new Utils.OktaClient();
+	const result = [];
+
+	const url = `api/v1/users/${id}/linkedObjects/${LINKED_OBJECT_NAME}Of`;
+
+	const response = await client.fetch({ url });
+
+	if (!response.ok) {
+		throw new ApiError({ statusCode: response?.statusCode, message: await response.json() });
+	}
+
+	const body = await response.json();
+
+	for (let i = 0; i < body.length; i++) {
+		const link = body[i];
+
+		const regex = /(?<=users\/).*/;
+
+		const {
+			_links: {
+				self: { href },
+			},
+		} = link;
+
+		const path = new URL(href).pathname;
+
+		const userId = regex.exec(path)[0] || undefined;
+
+		if (userId) {
+			const { id, profile } = await client.getUser(userId);
+
+			if (profile?.unifiedId === unifiedId) {
+				result.push({ id, ...(await Utils.cleanProfile(profile)) });
+			}
+		}
+	}
+
+	return result;
+};
 
 const getUser = async (req, res) => {
 	try {
@@ -34,23 +78,26 @@ const getUser = async (req, res) => {
 		}
 
 		const userProfile = user.profile;
-		const _linkedUsers = userProfile?.linkedUsers;
-		const unifiedId = userProfile?.unifiedId;
 
-		const linkedUsers = [];
+		const linkedUsers = await getLinkedProfiles(id, userProfile?.unifiedId);
 
-		if (Array.isArray(_linkedUsers) && _linkedUsers.length > 0) {
-			// Loop through linked users to fetch profiles.
-			for (let i = 0; i < _linkedUsers.length; i++) {
-				const { id, profile } = await client.getUser(_linkedUsers[i]);
+		// const _linkedUsers = userProfile?.linkedUsers;
+		// const unifiedId = userProfile?.unifiedId;
 
-				if (profile?.unifiedId === unifiedId) {
-					linkedUsers.push({ id, ...(await Utils.cleanProfile(profile)) });
-				}
-			}
-		}
+		// const linkedUsers = [];
 
-		const profile = { ...user.profile, linkedUsers };
+		// if (Array.isArray(_linkedUsers) && _linkedUsers.length > 0) {
+		// 	// Loop through linked users to fetch profiles.
+		// 	for (let i = 0; i < _linkedUsers.length; i++) {
+		// 		const { id, profile } = await client.getUser(_linkedUsers[i]);
+
+		// 		if (profile?.unifiedId === unifiedId) {
+		// 			linkedUsers.push({ id, ...(await Utils.cleanProfile(profile)) });
+		// 		}
+		// 	}
+		// }
+
+		const profile = { ...(await Utils.cleanProfile(user.profile)), linkedUsers };
 
 		return res.json({ ...user, profile });
 	} catch (error) {
