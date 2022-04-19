970aad174357a84f3fe6205e6887aecbab30f3e4
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-04-18 21:57:33 -0700

    refactor settings + add privacy toggle

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,64 +1,10 @@
 import { _, Auth, LDS, React, useState } from '../../common';
 
 import SettingsCard from '../../components/SettingsCard';
-import Account from '../../components/Account';
+import Providers from '../../components/Providers';
 import AddressForm from '../../components/AddressForm';
 
 import './styles.css';
-
-const Providers = () => {
-	const { user } = Auth.useAuthState();
-
-	const { credentials = [] } = user || {};
-
-	return (
-		<>
-			<Account
-				header='Salesforce Accounts'
-				subtitle={
-					<>
-						Connect your Salesforce login from any production org, Developer Edition org, or
-						Trailhead Playground. Learn more about{' '}
-						<a href='https://help.salesforce.com/articleView?id=tbid_account_merges.htm'>
-							merging your accounts.
-						</a>
-					</>
-				}
-				type='salesforce'
-				credentials={
-					credentials?.length > 0
-						? credentials.filter(
-								({ provider: { name: providerName } }) => providerName === 'salesforce'
-						  )
-						: []
-				}
-			/>
-			<Account
-				header='Email Accounts'
-				subtitle='Manage your connected email accounts.'
-				type='email'
-				credentials={
-					credentials?.length > 0
-						? credentials.filter(({ provider: { name: providerName } }) => providerName === 'email')
-						: []
-				}
-			/>
-			<Account
-				header='Social Accounts'
-				subtitle="Log in with your favorite social media accounts. Don't worry-we won't share your data or post anything on your behalf."
-				type='social'
-				credentials={
-					credentials?.length > 0
-						? credentials.filter(
-								({ provider: { name: providerName } }) =>
-									providerName !== 'email' && providerName !== 'salesforce'
-						  )
-						: []
-				}
-			/>
-		</>
-	);
-};
 
 const menuItems = [
 	{
@@ -90,7 +36,6 @@ const menuItems = [
 
 const Settings = () => {
 	const ref = React.useRef(null);
-	const { isLoadingLinkProfile } = Auth.useAuthState();
 
 	const [selectedItem, setSelectedItem] = useState('privacy');
 
@@ -128,7 +73,13 @@ const Settings = () => {
 						<SettingsCard
 							header='Privacy'
 							subheader='Choose how others see your profile.'
-							content='{{insert switch here}}'
+							content={
+								<LDS.Checkbox
+									labels={{ label: 'Public Profile' }}
+									id='public-profile-toggle'
+									variant='toggle'
+								/>
+							}
 						/>
 						<SettingsCard
 							header='Connected Accounts'
