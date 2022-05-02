e33d9c97a955878da190e5692d52547a623b9fda
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-05-02 14:51:14 -0700

    fix build scripts

--- ignore-step.sh
+++ ignore-step.sh
@@ -4,11 +4,10 @@ echo "ENV: $VERCEL_ENV"
 echo "BUILD BRANCH: $VERCEL_GIT_COMMIT_REF"
 echo "PROJECT BRANCH: $PROJECT_BRANCH"
 
-if [["$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" == "$PROJECT_BRANCH"]] ; then
+if [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" == "$PROJECT_BRANCH" ]] ; then
 	# Proceed with the build
 	echo "✅ - Build can proceed"
   	exit 1;
-
 else
 	# Don't build
 	echo "🛑 - Build cancelled"
