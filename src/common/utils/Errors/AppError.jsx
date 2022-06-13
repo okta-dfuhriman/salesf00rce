ecb1b0c318392ab2d071d6a8b86fb7c45e901e96
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-06-13 11:18:54 -0700

    consolidate errors

--- src/common/utils/Errors/AppError.jsx
+++
@@ -1,9 +0,0 @@
-export default class AppError extends Error {
-	constructor({ message, type, error }) {
-		super(message ?? 'AppError');
-
-		this.type = type;
-		this.name = 'AppError';
-		this.error = error;
-	}
-}
